f6fe7bb89729634be8bc4915efabe7fceb7e2fd8
Jacob Griswold-Moran <jgriswold@healthverity.com>
2019-08-06 15:46:54 -0400

    adds crypto module from hfc

---
+++ snakeskin/_hfc/__init__.py
@@ -0,0 +1,5 @@
+"""
+    This sub-package contains borrowed code from the official
+    Hyperledger Fabric Python SDK. Each individual module is unmodified
+    and is made available under the license in the respective file.
+"""
